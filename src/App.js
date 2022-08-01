6f5ae19de648b1c6249616237340557358b86028
Mayra Ibarra <mayraibarra@Mayras-MacBook-Pro.local>
2022-08-01 13:36:40 -0500

    created Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 import coverImage from "./assets/cover/techbg.jpg";
 
@@ -6,10 +7,13 @@ function App() {
 
   return (
     <div>
+      <Nav></Nav>
+      <main>
       <section className="my-5">
         <h1 id="about">Who am I?</h1>
         <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       </section>
+      </main>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+function Nav(){
+    return (
+        <header>
+<header>
+  <h2>
+    <a href="/">
+      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+    </a>
+  </h2>
+  <nav>
+    <ul className="flex-row">
+      <li className="mx-2">
+        <a href="#about">
+          About me
+        </a>
+      </li>
+      <li>
+        <span>Contact</span>
+      </li>
+    </ul>
+  </nav>
+</header>
+        </header>
+    );
+}
+export default Nav;
