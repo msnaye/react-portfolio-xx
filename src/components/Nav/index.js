19436fedd075e527f08605f4b795d30e7f00c301
msnaye <99111936+msnaye@users.noreply.github.com>
2022-08-02 00:08:17 -0500

    worked on About, Contact, Nav and added pictures

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
-function Nav(){
-    return (
-        <header>
-<header>
-  <h2>
-    <a href="/">
-      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-    </a>
-  </h2>
-  <nav>
-    <ul className="flex-row">
-      <li className="mx-2">
-        <a href="#about">
-          About me
+
+function Nav() {
+
+  return (
+
+    <header>
+      <h2>
+        <a href="/">
+          <h1 > Mayra Ibarra </h1>
         </a>
-      </li>
-      <li>
-        <span>Contact</span>
-      </li>
-    </ul>
-  </nav>
-</header>
-        </header>
-    );
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">
+              About Me
+            </a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+  );
 }
 export default Nav;
