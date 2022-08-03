feb1cfb73f78075a21f536217b31968a10e0dd75
msnaye <99111936+msnaye@users.noreply.github.com>
2022-08-03 01:32:44 -0500

    worked on Navbar

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
 import { capitalizeFirstLetter } from '../../utils/helpers'
 
 function Navbar() {
-  
+
   return (
     <div className='fixed w-full'>
 
       <header>
         <h2>
-          <a>
+          <p>
             <h1 > Mayra Ibarra </h1>
-          </a>
+          </p>
         </h2>
+        {/* Menu */}
         <nav>
           <ul className="flex-row">
             <li className="mx-2">
@@ -21,13 +23,13 @@ function Navbar() {
               </a>
             </li>
             <li>
-              <a href="#contact">
-                Contact
+              <a href="#portfolio">
+                Portfolio
               </a>
             </li>
             <li>
-              <a href="#portfolio">
-                Portfolio
+              <a href="#contact">
+                Contact
               </a>
             </li>
             <li>
@@ -36,6 +38,18 @@ function Navbar() {
               </a>
             </li>
           </ul>
+          {/* Hamburger Menu */}
+          <div className='hiddend'><FaBars />
+          </div>
+          {/* Mobile Menu */}
+          <ul className='hidden'>
+            <li>About</li>
+            <li>Portfolio</li>
+            <li>Resume</li>
+            <li>Contact</li>
+            <li></li>
+          </ul>
+
         </nav>
       </header>
     </div>
